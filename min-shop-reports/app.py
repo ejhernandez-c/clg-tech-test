578ac884d6506519843cda52c4c5bb42234ec0c7
3m1l10H <89489574+3m1l10H@users.noreply.github.com>
2025-10-17 00:46:55 -0600

    replacing queries with sps calls

--- min-shop-reports/app.py
+++ min-shop-reports/app.py
@@ -23,57 +23,29 @@ def index():
 
 @app.route('/reports/top-customers')
 def top_customers():
-    sql = """
-    SELECT 
-      o.customer_id,
-      c.name AS customer_name,
-      COUNT(DISTINCT o.id) AS total_orders,
-      ROUND(SUM(oi.quantity * oi.unit_price),2) AS total_spend,
-      ROUND(SUM(oi.quantity * oi.unit_price)/COUNT(DISTINCT o.id),2) AS avg_order_value
-    FROM 
-    clgdb.orders o
-      INNER JOIN clgdb.order_items oi 
-        ON oi.order_id = o.id
-      INNER JOIN clgdb.customers c 
-        ON c.id = o.customer_id 
-    WHERE  
-      o.status ='PAID'
-    AND o.created_at >= NOW() - INTERVAL 30 DAY
-    GROUP BY o.customer_id,c.name 
-     ORDER BY total_spend DESC
-    LIMIT 5;
+    """
+    Call stored procedure clgdb.get_top_customers_orders()
+    Returns the top 5 customers by total spend.
     """
     conn = get_connection()
     with conn.cursor() as cur:
-        cur.execute(sql)
+        cur.callproc('clgdb.get_top_customers_orders')
         rows = cur.fetchall()
     conn.close()
     return jsonify(rows)
 
 @app.route('/reports/top-products')
 def top_products():
-    sql = """
-  SELECT
-      oi.product_sku,
-      SUM(oi.quantity) AS total_units_sold,
-      ROUND(SUM(oi.quantity * oi.unit_price), 2) AS total_revenue,
-      COUNT(DISTINCT o.customer_id) AS unique_customers,
-      ROUND(SUM(oi.quantity * oi.unit_price) / COUNT(DISTINCT oi.order_id), 2) AS avg_order_value_per_product
-    FROM clgdb.orders o
-      INNER JOIN clgdb.order_items oi 
-    	ON o.id = oi.order_id
-    WHERE o.status = 'PAID'
-      AND o.created_at >= NOW() - INTERVAL 60 DAY
-    GROUP BY oi.product_sku
-    ORDER BY total_revenue DESC
-    LIMIT 5;
-    """
-    conn = get_connection()
-    with conn.cursor() as cur:
-        cur.execute(sql)
-        rows = cur.fetchall()
-    conn.close()
-    return jsonify(rows)
+  """
+    Call stored procedure clgdb.get_top_products()
+    Returns the top 5 products by total revenue.
+  """
+  conn = get_connection()
+  with conn.cursor() as cur:
+      cur.callproc('clgdb.get_top_products')
+      rows = cur.fetchall()
+  conn.close()
+  return jsonify(rows)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
